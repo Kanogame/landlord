0e7e9047f11288ceb89ec7452274abbd80705a64
kanogame <wasdqert1222@gmail.com>
2025-05-21 13:27:30 +0700

    icons & lisepagelink

--- app/blocks/mobileSidepage.tsx
+++ app/blocks/mobileSidepage.tsx
@@ -1,4 +1,15 @@
 import { AnimatePresence, motion } from "motion/react";
+import SidePageLink from "~/components/sidePageLink";
+import iconHouse from "~/media/icons/icon-house.svg";
+import iconSearch from "~/media/icons/icon-search.svg";
+import iconLink from "~/media/icons/icon-link.svg";
+import iconChat from "~/media/icons/icon-chat.svg";
+import iconHistory from "~/media/icons/icon-history.svg";
+import iconBookmark from "~/media/icons/icon-bookmark.svg";
+import iconCalendar from "~/media/icons/icon-calendar.svg";
+import iconGear from "~/media/icons/icon-gear.svg";
+import iconTeam from "~/media/icons/icon-team.svg";
+import iconHelp from "~/media/icons/icon-help.svg";
 
 const variants = {
   enter: () => {
@@ -30,9 +41,18 @@ export default function SidePage() {
         type: "tween",
         duration: 0.2,
       }}
-      className="absolute bg-amber-600 w-[100vw] h-[100vh] z-100"
+      className="absolute cursor-pointer bg-white w-[100vw] h-[100vh] z-100 flex flex-col gap-[5px] items-start"
     >
-      hello
+      <SidePageLink label="Главная" img={iconLink} />
+      <SidePageLink label="Поиск" img={iconSearch} />
+      <SidePageLink label="Сообщения" img={iconChat} />
+      <SidePageLink label="Мои объявления" img={iconHouse} />
+      <SidePageLink label="Предыдущая недвижимость" img={iconHistory} />
+      <SidePageLink label="Закладки" img={iconBookmark} />
+      <SidePageLink label="Мониторинг" img={iconCalendar} />
+      <SidePageLink label="Настройки профиля" img={iconGear} />
+      <SidePageLink label="Обратная связь" img={iconTeam} />
+      <SidePageLink label="Помощь" img={iconHelp} />
     </motion.div>
   );
 }
---
+++ app/components/sidePageLink.tsx
@@ -0,0 +1,23 @@
+import { motion } from "motion/react";
+
+export default function SidePageLink(props: {
+  img: string;
+  label: string;
+  onClick?: () => void;
+}) {
+  function handleClick() {
+    if (props.onClick) {
+      props.onClick();
+    }
+  }
+
+  return (
+    <motion.div
+      onClick={handleClick}
+      className="h5-def sidepage flex items-center justify-center gap-[6px] px-[15px] py-[5px] rounded-[8px]"
+    >
+      <img src={props.img} alt="" />
+      <div className="text-[14px]/[16px]">{props.label}</div>
+    </motion.div>
+  );
+}
